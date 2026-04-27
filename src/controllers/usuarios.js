583c18ed97a38db96bad4213d33942acfe0b3e2e
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-04-27 19:59:53 -0300

    update

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -73,11 +73,46 @@ module.exports = {
     },
     async editarUsuarios(request, response) {
         try {
+
+            const { nome, email, senha, tipo, dt_nasc, cpf, ativo } = request.body; 
+            const { id } = request.params; 
+
+            const sql = `
+                UPDATE usuarios SET 
+                    usu_nome = ?, 
+                    usu_email = ?, 
+                    usu_senha = ?, 
+                    usu_dt_nasc = ?, 
+                    usu_cpf = ?, 
+                    usu_tipo = ?, 
+                    usu_ativo = ? 
+                WHERE usu_id = ?;                 
+            `;
+
+            const values = [nome, email, senha, dt_nasc, cpf, tipo, ativo, id]; 
+
+            const [result] = await db.query(sql, values); 
+
+            if (result.affectededRows === 0) {
+                return response.status(404).json({
+                    sucesso: false, 
+                    mensagem: `Usuário ${id} não encontrado!`, 
+                    dados: null
+                })
+            }
+
+            const dados = {
+                id, 
+                nome, 
+                email,
+                tipo
+            }            
+
             return response.status(200).json(
                 {
                     sucesso: true,
-                    mensagem: 'Atualização de usuário realizada com sucesso',
-                    dados: null
+                    mensagem: `Usuário ${id} atualizado com sucesso!`,
+                    dados
                 }
             );
         } catch (error) {
--- src/routes/ewerton.js
+++ src/routes/ewerton.js
@@ -11,7 +11,7 @@ const ProdutoIngredientes = require('../controllers/produtoIngredientes');
 
 router.get('/usuarios', UsuariosController.listarUsuarios); 
 router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
-router.patch('/usuarios', UsuariosController.editarUsuarios);
+router.patch('/usuarios/:id', UsuariosController.editarUsuarios);
 router.delete('/usuarios', UsuariosController.apagarUsuarios);
 
 router.get('/produtos', ProdutosController.listarProdutos); 
